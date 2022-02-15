37683f320b1ce6d0e4dca46f05e7f08a530376b4
Tony <tony@saagie.com>
2022-02-15 15:42:13 +0100

    Improved runtime day 10

--- 2020/day10_adapter_array.R
+++ 2020/day10_adapter_array.R
@@ -1,8 +1,8 @@
 a <- strtoi(rfp('2020','10'))
-a2 <- a[collapse::radixorder(a)] # Sort numbers
+a2 <- radsort(a) # Sort numbers
 
 diffs <- c(a2, a2[length(a2)]+3L)-c(0L, a2) # Differences with previous number
-prod(Rfast::Table(diffs)) # Part 1
+prod(kit::countOccur(diffs)$Count) # Part 1 (2738): Product of frequencies of 3's and 1's
 
 # Fast run length
 frl <- \(x) {
@@ -12,7 +12,7 @@ frl <- \(x) {
 }
 
 groups <- frl(diffs) # Compute length of consecutive 1s and 3s
-groups <- Rfast::Table(groups[seq.int(1L, length(groups), 2L)]) # Frequencies of sequences of 1s
-
-res <- 2^groups['2'] * 4^groups['3'] * 7^groups['4'] # Number of paths for each type of group
-res # Part 2
+g <- groups[seq.int(1L, length(groups), 2L)] # Keep only lengths of 1's
+# 2 1's in a row multiply possibilities by 2, 3 1's by 4 and 4 1's by 7
+res <- 2^kit::count(g, 2L) * 4^kit::count(g, 3L) * 7^kit::count(g, 4L) # Multiply all number of possibilities 
+res # Part 2 (74049191673856): All possibles ways to connect the adapters
