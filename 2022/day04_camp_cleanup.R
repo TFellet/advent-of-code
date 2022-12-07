98ea5f36d4a8cee42636a0c7462c87970d599730
Tony <tony@saagie.com>
2022-12-07 14:42:04 +0100

    Simpler day 4

--- 2022/day04_camp_cleanup.R
+++ 2022/day04_camp_cleanup.R
@@ -3,4 +3,4 @@ a <- brio::read_file(fp('2022','4')) |> # Read input as a single string
   scan(text = _, what=list(0L,0L,0L,0L),sep = '-') # Parse string into 4 lists of numbers with scan
 collapse::massign(c('w', 'x', 'y', 'z'), a) # Assign simpler names
 sum((w<=y & x>=z) | (y<=w & z>=x)) # Part 1 (433): Number of complete overlaps
-sum((w>=y & w<=z) | (x>=y & x<=z) | (y>=w & y<=x) | (z>=w & z<=x)) # Part 2 (852): Number of partial overlaps
+sum((w<=z & x>=y)) # Part 2 (852): Number of partial overlaps
