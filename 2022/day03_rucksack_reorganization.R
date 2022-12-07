89d1419ea7748dc994c3a9ff189512a279a6ddbe
Tony <tony@saagie.com>
2022-12-07 14:41:46 +0100

    Faster day 3

--- 2022/day03_rucksack_reorganization.R
+++ 2022/day03_rucksack_reorganization.R
@@ -1,11 +1,9 @@
-a <- rfp('2022','3')
-comp1 <- substr(a, 1, nchar(a)/2) |> strsplit('', fixed = T)
-comp2 <- substr(a, nchar(a)/2+1, nchar(a)) |> strsplit('', fixed = T)
-inter <- mapply(intersect, comp1, comp2) |> paste0(collapse = '')
-prio <- utf8ToInt(inter)
-sum(prio[prio > 96] - 96) + sum(prio[prio < 91] - 38) # Part 1 (8105)
+a <- rfp('2022','3') |> strsplit('',fixed=T) # Read input and split into character vectors
+ints <- lapply(a, fastmatch::fmatch, c(letters, LETTERS)) # Convert letters to integers
+inter <- lapply(ints, matrix, ncol=2) |> sapply(\(x) x[,1][x[,1] %in% x[,2]][1]) # Split into 2 part using matrix and find intersection
+sum(inter) # Part 1 (8105): Sum of letters found
 
-gr <- frepEach(1:(length(a)/3), 3) # Groups id
-findBadge <- \(x) lapply(x, stringi::stri_count_fixed, let) |> Reduce(f=`&`) |> which() # Function to find a badge in a group
-badges <- collapse::BY(a, g=gr, findBadge) # Find all badges
-sum(badges) # Part 2 (2363)
+tabs <- lapply(ints, tabulate, nbins = 26*2) # Convert single int to long vector
+findBadge <- \(x) Reduce(x, f=`&`) |> which.max() # Function to find a badge in a group
+badges <- matrix(tabs, ncol = 3, byrow = T) |> collapse::mrtl() |> sapply(findBadge) # Split input into groups using matrix and find all badges
+sum(badges) # Part 2 (2363): Sum of all badges
