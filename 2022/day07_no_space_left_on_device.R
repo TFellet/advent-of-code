3d8294032f188ef300f952321c0382713f89f730
Tony <tony@saagie.com>
2022-12-21 14:25:24 +0100

    day 7 with hashmap to improve perf

--- 2022/day07_no_space_left_on_device.R
+++ 2022/day07_no_space_left_on_device.R
@@ -1,8 +1,7 @@
 a <- rfp('2022','7') # Read input
 
 path <- 'root' # Initialize the current path to the root directory
-dir_size <- list() # Initialize an empty list to store the size of each directory
-
+direnv <- new.env(parent = emptyenv()) # Initialize an empty hashmap to store the size of each directory
 # For each line in the file system data
 for (spl in strsplit(a, ' ', fixed = T)) {
   if(spl[1] == '$') { # Line is a command
@@ -15,19 +14,20 @@ for (spl in strsplit(a, ' ', fixed = T)) {
     }
   } else if(spl[1] != 'dir') { # If the line is not a directory
     tmpath <- path # Create a temporary variable to store the current path
+    size <- strtoi(spl[1])
     # Iterate over each directory in the path from the current directory up to the root
     while (tmpath != '.') {
       # Initialize the size of the current directory in the path if it hasn't been seen yet
-      if(length(dir_size[[tmpath]]) == 0) dir_size[[tmpath]] <- 0L
-      # Add the size of the current file to the size of the current directory in the path
-      dir_size[[tmpath]] <- dir_size[[tmpath]] + strtoi(spl[1])
+      if(is.null(direnv[[tmpath]])) direnv[[tmpath]] <- size else
+        # Add the size of the current file to the size of the current directory in the path
+        direnv[[tmpath]] <- direnv[[tmpath]] + size
       tmpath <- dirname(tmpath) # Move up to the parent directory in the path
     }
   }
 }
 
-dir_size2 <- unlist(dir_size) # Flatten the directory sizes into a single vector
-sum(dir_size2[dir_size2 < 100000]) # Part 1 (1307902): sum of the sizes of all directories smaller than 100000
+dir_size <- unlist(as.list(direnv)) # Flatten the directory sizes into a single vector
+sum(dir_size[dir_size < 100000]) # Part 1 (1307902): sum of the sizes of all directories smaller than 100000
 
-goal_dir <- 30000000 - 70000000 + dir_size2['root'] # Target size to free
-min(dir_size2[dir_size2 > goal_dir]) # Part 2 (7068748): Smallest folder to delete to free up enough space
+goal_dir <- 30000000 - 70000000 + dir_size['root'] # Target size to free
+min(dir_size[dir_size > goal_dir]) # Part 2 (7068748): Smallest folder to delete to free up enough space
