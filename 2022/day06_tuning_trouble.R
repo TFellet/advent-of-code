cb8ee148517ef635c727850f3e27e82a27d8cfc1
Tony <tony@saagie.com>
2022-12-21 17:38:45 +0100

    Day 6 improvements first part with a regex second part skipping multiple steps when possible

--- 2022/day06_tuning_trouble.R
+++ 2022/day06_tuning_trouble.R
@@ -1,14 +1,18 @@
-a <- strsplit(rfp('2022','6'), "", fixed=T)[[1]] # Read input string
+a <- rfp('2022','6')
+a2 <- strsplit(a, "", fixed=T)[[1]] # Read input string
+reg <- r'[(.)(?!\1)(.)(?!\1|\2)(.)(?!\1|\2|\3)(.)]' # Regex is faster for part 1
 
-# Define a function to detect the first start-of-message marker
 findStart <- function(datastream, num_chars = 4) {
-  # Loop through the datastream, one character at a time
-  for (count in num_chars:length(datastream)) {
-    # Check if the last 14 characters are all different
-    if (length(unique(datastream[(count-num_chars+1):count])) == num_chars) 
-      return(count) # If they are, return the current value of the counter
+  count <- 1L # First position to check
+  repeat {
+    chars <- datastream[count:(count+num_chars-1)] # Elements to check
+    # Find which elements are duplicated and where; It tells us how many positions we can skip
+    matches <- match(chars, chars)
+    dup <- matches[matches != seq_len(num_chars)] # Check if all characters are found only at their positions
+    # If all characters are unique, returns end position, otherwise increase count where the last duplicate found won't be present
+    if(length(dup) == 0L) return(count + num_chars - 1) else count <- count + max(dup)
   }
 }
 
-findStart(a, 4) # Part 1 (1361): First occurence of 4 differents characters
-findStart(a, 14) # Part 2 (3263): First occurence of 14 differents characters
+regexec(reg, a, perl=T)[[1]] |> flast() # Part 1 (1361): First occurence of 4 differents characters
+findStart(a2, 14) # Part 2 (3263): First occurence of 14 differents characters
