e5ee3b5acd1305b80968bc0e16ee9181de59760e
Tony <tony@saagie.com>
2025-12-09 15:21:48 +0100

    2025 new solutions days 7 and 8

--- 2025/day07_laboratories.py
+++ 2025/day07_laboratories.py
@@ -14,3 +14,19 @@ for r in data[1:]:
             row_beams[beam] += beams[beam]
     beams = row_beams
 p1, sum(beams.values())
+
+# Alternative solution using a single array instead of a dict
+data = open("2025/inputs/day7.txt").readlines()
+paths = [0] * len(data[0])
+paths[data[0].index("S")] = 1
+p1 = 0
+for r in data[1:]:
+    if (st := r.find("^")) == -1:
+        continue
+    for i in range(st, len(paths)):
+        if r[i] == "^":
+            p1 += paths[i] > 0
+            paths[i - 1] += paths[i]
+            paths[i + 1] += paths[i]
+            paths[i] = 0
+p1, sum(paths) # (1638, 7759107121385)
--- 2025/day08_playground.py
+++ 2025/day08_playground.py
@@ -1,10 +1,15 @@
 import math
 from collections import Counter
 
+from scipy.cluster.hierarchy import DisjointSet
+
 data = open("2025/inputs/day8.txt").readlines()
 # Read points into int tuples
-data_int = [(int(s[0]), int(s[1]), int(s[2])) for r in data if (s := r.split(","))]
+data_int = [tuple(map(int, r.split(","))) for r in data]
 le = len(data_int)
+
+## Approach 1: Dictionnary implementation
+
 # Compute distance between all possible pairs of points and sort them
 dist_list = []
 for i in range(le):
@@ -31,5 +36,83 @@ for p, _ in sdist:  # p is the pair of points connected
     j -= 1  # Number of distinct circuits left
     if j == 1:  # When there's only one circuit left, multipy the X coordinate of the last 2 points connected
         p2 = data_int[p[0]][0] * data_int[p[1]][0]
+        break
+p1, p2
+
+
+## Approach 2: Using scipy's disjoint set implementation
+
+dist_list2 = []
+for i in range(le):
+    for j in range(i + 1, le):
+        dist_list2.append(((data_int[i], data_int[j]), math.dist(data_int[i], data_int[j])))
+sdist2 = [x[0] for x in sorted(dist_list2, key=lambda x: x[1])]
+
+p1 = p2 = i = 0
+dset = DisjointSet(data_int)
+for po1, po2 in sdist2:
+    i += 1
+    dset.merge(po1, po2)
+    if i == 1000:
+        p1 = math.prod(sorted((len(s) for s in dset.subsets()), reverse=True)[:3])
+    if dset.n_subsets == 1:
+        p2 = po1[0] * po2[0]
+        break
+p1, p2
+
+
+
+## Approach 3: Implementing a custom disjoint set implementation (slighty faster than scipy for some reason)
+
+
+class disSet:
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.n = len(vertices)  # Number of trees
+        self.parent = {i: i for i in vertices}  # Each item is its own parent at the start
+        self.ranks = {i: 0 for i in vertices}  # All trees are rank 0
+
+    def __getitem__(self, item):
+        """Find the root of the set that contains item, while flattening the tree on the way"""
+        if self.parent[item] != item:  # If the item is not at the top of the tree
+            self.parent[item] = self[self.parent[item]]  # Update it's parent to be the top of the tree
+        return self.parent[item]  # Return the root of the tree
+
+    def union(self, set1, set2):
+        """Union two sets together by rank"""
+        root1 = self[set1]
+        root2 = self[set2]
+        if root1 != root2:  # If the 2 items have different roots, they need to be merged
+            # Merge smaller ranked tree into larger one
+            if self.ranks[root1] < self.ranks[root2]:
+                self.parent[root1] = root2
+            else:
+                self.parent[root2] = root1
+                if self.ranks[root1] == self.ranks[root2]:  # If both tree have the same rank, increase rank by 1
+                    self.ranks[root1] += 1
+            self.n -= 1  # When 2 sets have been merged, there is 1 less set
+
+    def flatten(self):
+        """Flatten all trees to get an accurate size of each set when looking at its parent"""
+        for i in self.vertices:
+            self[i]
+        return self.parent
+
+
+dist_list2 = []
+for i in range(le):
+    for j in range(i + 1, le):
+        dist_list2.append(((data_int[i], data_int[j]), math.dist(data_int[i], data_int[j])))
+sdist2 = [x[0] for x in sorted(dist_list2, key=lambda x: x[1])]
+
+dis_set = disSet(data_int)
+p1 = p2 = i = 0
+for po1, po2 in sdist2:
+    i += 1
+    dis_set.union(po1, po2)
+    if i == 1000:
+        p1 = math.prod(x for _, x in Counter(dis_set.flatten().values()).most_common(3))
+    if dis_set.n == 1:
+        p2 = po1[0] * po2[0]
         break
 p1, p2
