067f08f58e7ee775c997ba8b5e16b6b615e6e219
Tony <tony@saagie.com>
2025-12-11 11:59:04 +0100

    2025 day 11 use matrix exponent

--- 2025/day11_reactor.py
+++ 2025/day11_reactor.py
@@ -16,17 +16,12 @@ for s, d in zip(source, dest):
 
 def count_paths(mat, ids, start_id, dest_id):
     start = np.zeros(len(ids))
-    start[ids[start_id]] = 1
+    start[ids[start_id]] = 1 # Filter paths starting at start id
     m = mat.copy()
-    m[:, ids[dest_id]] = 0
-    npaths = 0
-    for i in range(len(ids)):
-        m = m.__matmul__(mat)
-        if not np.any(m):
-            break
-        res = m.__matmul__(start)[:, ids[dest_id]].sum()
-        npaths += res
-    return int(npaths)
+    m[:, ids[dest_id]] = 0 # No exit after reaching destination
+    m[ids[dest_id],ids[dest_id]] = 1 # Once at destination, stay there
+    # Take the transition matrix after n steps and find how many paths lead to destination
+    return int(np.linalg.matrix_power(m, len(ids)).__matmul__(start)[:, ids[dest_id]].sum())
 
 count_paths(mat, ids, "you", "out")  # 688
 (
